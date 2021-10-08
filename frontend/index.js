711a34d50b3207d5bc0c00a83e565037c5286b12
peter-alethio <peter@alethio.io>
2021-10-08 17:14:56 +0900

    feat: Todo 모듈 생성
    - todo 본문에 사용될 모듈

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,14 @@
 new (function() {
 
     function init() {
-
+        this.todo = new Todo({
+            title: "todo 모듈"
+        });
     }
 
     init();
 })();
+
+function Todo({title}) {
+    console.log(title)
+}
