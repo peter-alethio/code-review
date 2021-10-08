11e94d7e8056885e2718ad6659839b82b78fd078
peter-alethio <peter@alethio.io>
2021-10-08 17:17:14 +0900

    feat: todo 모듈에 tag 주입
    - html에서 todo 본문을 렌더링할 tag 요소 이름

--- frontend/index.js
+++ frontend/index.js
@@ -2,13 +2,22 @@ new (function() {
 
     function init() {
         this.todo = new Todo({
-            title: "todo 모듈"
+            title: "todo 모듈",
+            tag: '#todo'
         });
     }
 
     init();
 })();
 
-function Todo({title}) {
-    console.log(title)
+function Todo({title, tag}) {
+
+    function init() {
+        this.title = title;
+        this.$tag = tag;
+
+        console.log(this.title, this.$tag)
+    }
+
+    init();
 }
