ab2443039873ef52bf15118e3ab298cc785ca4dd
peter-alethio <87297863+peter-alethio@users.noreply.github.com>
2021-10-13 10:56:40 +0900

    feat: todo 태그 이벤트 (#4)
    * feat: div 추가
    
    * feat: todo 모듈 렌더링
    
    * feat: 클릭 시, alert 메세지 표시
    
    * feat: input 태그 추가

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,29 @@
+function Todo ({title, tag}) {
+
+    const CLICK_EVENT = 'click';
+    const INPUT_ELEMENT = "input";
+
+    const init = () => {
+        this.title = title;
+        this.$tag = tag;
+
+        render();
+    }
+
+    const render = () => {
+        this.$todo = document.querySelector(this.$tag);
+        this.$todo.appendChild(document.createTextNode(this.title))
+        this.$todo.addEventListener(CLICK_EVENT, () => alert(this.title))
+
+        this.$todo.appendChild(document.createElement(INPUT_ELEMENT))
+    }
+
+    init();
+}
+
 new (function() {
 
-    function init() {
+    const init = () => {
         this.todo = new Todo({
             title: "todo 모듈",
             tag: '#todo'
@@ -10,14 +33,3 @@ new (function() {
     init();
 })();
 
-function Todo({title, tag}) {
-
-    function init() {
-        this.title = title;
-        this.$tag = tag;
-
-        console.log(this.title, this.$tag)
-    }
-
-    init();
-}
